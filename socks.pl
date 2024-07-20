1cb8ee1823be1855918f3cfcf784b6990bc3c794
bannsec <7632864+bannsec@users.noreply.github.com>
2024-07-20 02:17:30 +0000

    Re-wrote a bit and fixed

--- socks.pl
+++ socks.pl
@@ -3,6 +3,16 @@ use warnings;
 use IO::Socket::INET;
 use IO::Select;
 use POSIX qw(:signal_h);
+use Socket qw(inet_aton);
+
+
+# Add this debug function at the beginning of your script
+sub debug_log {
+    my ($message) = @_;
+    open my $log, '>>', 'socks_debug.log' or die "Could not open log file: $!";
+    print $log "$message\n";
+    close $log;
+}
 
 my $port = 1080;
 if (@ARGV && $ARGV[0] eq '-p' && $ARGV[1]) {
@@ -19,12 +29,12 @@ my $server = IO::Socket::INET->new(
     Reuse     => 1
 ) or die "Could not create socket: $!\n";
 
-print "SOCKS5 server is running and listening on port $port\n";
+debug_log("SOCKS5 server is running and listening on port $port");
 
 my $select = IO::Select->new($server);
 
 $SIG{INT} = sub {
-    print "Shutting down the server...\n";
+    debug_log("Shutting down the server...");
     close($server);
     exit(0);
 };
@@ -35,6 +45,7 @@ while (1) {
         if ($fh == $server) {
             my $client = $server->accept;
             $select->add($client);
+            debug_log("New client connected");
         } else {
             my $data;
             my $bytes_read = $fh->sysread($data, 1024);
@@ -43,6 +54,7 @@ while (1) {
             } else {
                 $select->remove($fh);
                 close($fh);
+                debug_log("Client disconnected");
             }
         }
     }
@@ -51,54 +63,117 @@ while (1) {
 sub handle_socks5 {
     my ($client, $data, $select) = @_;
 
+    debug_log("Handling SOCKS5 request");
+    debug_log("Initial data: " . unpack("H*", $data));
+
     # Initial SOCKS5 handshake
     my ($version, $nmethods) = unpack('C2', $data);
+    debug_log("SOCKS version: $version, Number of methods: $nmethods");
+
     if ($version != 5) {
+        debug_log("Invalid SOCKS version: $version");
         close($client);
         return;
     }
 
     my @methods = unpack("C$nmethods", substr($data, 2));
+    debug_log("Authentication methods: " . join(", ", @methods));
+
     if (!grep { $_ == 0 } @methods) {
+        debug_log("No acceptable authentication method");
         close($client);
         return;
     }
 
     $client->syswrite(pack('C2', 5, 0));
+    debug_log("Sent authentication response");
 
     # Read SOCKS5 request
-    $client->sysread($data, 4);
+    my $bytes_read = $client->sysread($data, 4);
+    debug_log("Read $bytes_read bytes for SOCKS5 request");
+    debug_log("SOCKS5 request data: " . unpack("H*", $data));
+
     my ($ver, $cmd, $rsv, $atyp) = unpack('C4', $data);
+    debug_log("SOCKS5 request - Version: $ver, Command: $cmd, Reserved: $rsv, Address type: $atyp");
+
     if ($ver != 5 || $cmd != 1) {
+        debug_log("Invalid SOCKS5 request");
         close($client);
         return;
     }
 
     my $addr;
-    if ($atyp == 1) {
-        $client->sysread($data, 6);
-        my ($ip, $port) = unpack('Nn', $data);
-        $addr = inet_ntoa(pack('N', $ip)) . ":$port";
-    } elsif ($atyp == 3) {
-        $client->sysread($data, 1);
-        my $len = unpack('C', $data);
-        $client->sysread($data, $len + 2);
-        my ($host, $port) = unpack("A$len n", $data);
-        $addr = "$host:$port";
-    } else {
+    eval {
+        if ($atyp == 1) {
+            debug_log("IPv4 address");
+            $bytes_read = $client->sysread($data, 4);
+            debug_log("Read $bytes_read bytes for IPv4 address");
+            debug_log("IPv4 address data: " . unpack("H*", $data));
+            my $ip_packed = $data;
+            my $ip = join('.', unpack("C4", $ip_packed));
+            debug_log("Unpacked IP: $ip");
+            $bytes_read = $client->sysread($data, 2);
+            debug_log("Read $bytes_read bytes for port");
+            debug_log("Port data: " . unpack("H*", $data));
+            my $port = unpack('n', $data);
+            debug_log("Unpacked port: $port");
+            $addr = "$ip:$port";
+        } elsif ($atyp == 3) {
+            debug_log("Domain name");
+            $bytes_read = $client->sysread($data, 1);
+            debug_log("Read $bytes_read byte for domain name length");
+            my $len = unpack('C', $data);
+            debug_log("Domain name length: $len");
+            $bytes_read = $client->sysread($data, $len);
+            debug_log("Read $bytes_read bytes for domain name");
+            debug_log("Domain name data: " . unpack("H*", $data));
+            my $host = unpack("A$len", $data);
+            debug_log("Unpacked host: $host");
+            $bytes_read = $client->sysread($data, 2);
+            debug_log("Read $bytes_read bytes for port");
+            debug_log("Port data: " . unpack("H*", $data));
+            my $port = unpack('n', $data);
+            debug_log("Unpacked port: $port");
+            $addr = "$host:$port";
+        } else {
+            die "Unsupported address type: $atyp";
+        }
+    };
+    if ($@) {
+        debug_log("Error processing address: $@");
         close($client);
         return;
     }
 
+    debug_log("Target address: $addr");
+
     # Connect to target server
     my $target = IO::Socket::INET->new(PeerAddr => $addr, Proto => 'tcp');
     if (!$target) {
+        debug_log("Failed to connect to target server");
         $client->syswrite(pack('C4', 5, 5, 0, 1) . pack('Nn', 0, 0));
         close($client);
         return;
     }
 
-    $client->syswrite(pack('C4', 5, 0, 0, 1) . pack('Nn', inet_aton($target->peerhost), $target->peerport));
+    debug_log("Connected to target server");
+    
+    my $response;
+    eval {
+        my $ip_addr = inet_aton($target->peerhost);
+        my $port = $target->peerport;
+        debug_log("Response IP: " . unpack("H*", $ip_addr) . ", Port: $port");
+        $response = pack('C4', 5, 0, 0, 1) . $ip_addr . pack('n', $port);
+    };
+    if ($@) {
+        debug_log("Error creating response: $@");
+        close($client);
+        close($target);
+        return;
+    }
+    
+    debug_log("Response to client: " . unpack("H*", $response));
+    $client->syswrite($response);
 
     # Add target to select
     $select->add($target);
@@ -107,8 +182,11 @@ sub handle_socks5 {
     relay_data($client, $target, $select);
 }
 
+
+
 sub relay_data {
     my ($client, $target, $select) = @_;
+    debug_log("Starting data relay");
     while (1) {
         my @ready = $select->can_read;
         foreach my $fh (@ready) {
@@ -117,10 +195,13 @@ sub relay_data {
             if ($bytes_read) {
                 if ($fh == $client) {
                     $target->syswrite($data);
+                    debug_log("Relayed data from client to target");
                 } else {
                     $client->syswrite($data);
+                    debug_log("Relayed data from target to client");
                 }
             } else {
+                debug_log("Connection closed");
                 $select->remove($client);
                 $select->remove($target);
                 close($client);
